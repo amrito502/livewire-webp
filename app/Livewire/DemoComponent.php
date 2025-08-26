2d9244787ad44d03b75c2531d54c6c079a979a2e
amrito502 <amritobosu7@gmail.com>
2025-08-26 17:56:46 +0600

    make counter

--- app/Livewire/DemoComponent.php
+++ app/Livewire/DemoComponent.php
@@ -7,6 +7,15 @@ use Livewire\Component;
 class DemoComponent extends Component
 {
     public $name = "mike";
+
+    public $count_value = 0;
+
+    public function increment(){
+        $this->count_value++;
+    }
+    public function decrement(){
+        $this->count_value--;
+    }
     public function render()
     {
         return view('livewire.demo-component',[
--- resources/views/livewire/demo-component.blade.php
+++ resources/views/livewire/demo-component.blade.php
@@ -1,3 +1,7 @@
 <div>
-    hello from livewire {{$name}} - {{ $age }}
+    hello from livewire {{$name}} - {{ $age  }}
+
+    <h2>Count : {{ $count_value }}</h2>
+    <button wire:click="increment">Increment</button>
+    <button wire:click="decrement">Decrement</button>
 </div>
